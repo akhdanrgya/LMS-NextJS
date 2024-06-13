74f302ff60f1b6edf228753ffed75f4932432264
akhdanrgya <akhdan.anargya@gmail.com>
2024-06-14 00:13:06 +0700

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/navbar"
+
+const Home = () => {
+  return (
+    <main>
+      <Navbar/>
+      <div>
+        <h1>
+          Home Page
+        </h1>
+      </div>
+    </main>
+  )
+}
+
+export default Home
