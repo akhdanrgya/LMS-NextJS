54f33b6c1e6795b463b500530b5dd654a716c48d
akhdan.anargya <akhdan.anargya@gmail.com>
2024-06-18 14:20:52 +0000

    Onboarding

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -107,7 +107,7 @@ const Navbar: React.FC = () => {
                 aria-labelledby="user-menu-button" 
                 tabIndex={-1}
               >
-                <Link href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</Link>
+                <Link href="/onboarding/profile" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</Link>
                 <Link href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</Link>
                 <Link href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-2">Sign out</Link>
               </div>
