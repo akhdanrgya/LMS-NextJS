ec9b4c85df36b95a902d46279b1c82edd8bc14d7
akhdanrgya <akhdan.anargya@gmail.com>
2024-06-21 05:14:16 +0700

    Update card

--- src/app/course/myCourse.tsx
+++ src/app/course/myCourse.tsx
@@ -1,10 +1,23 @@
-const MyCourse = () => {
-    return (
-        <div className="bg-gray-100 p-4 mb-8">
-        <button className="font-mono hover:text-blue-800 cursor-pointer text-lg mb-4">Add Course</button>
-        <p>Belum ada course</p>
-        </div>
-    )
-}
+'use client'
 
-export default MyCourse
+import { Card, CardBody } from "@nextui-org/card";
+import { useState } from "react";
+
+const MyCourse = () => {
+  return (
+    <div className="bg-gray-100 p-4 mb-8">
+      <button className="font-mono hover:text-blue-800 cursor-pointer text-lg mb-4">
+        Add Course
+      </button>
+
+      <Card>
+        <CardBody>
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+      </Card>
+
+    </div>
+  );
+};
+
+export default MyCourse;
