38d4a6f6c7001db22bc8a4bdacfa088b44349c5f
akhdanrgya <akhdan.anargya@gmail.com>
2024-06-21 05:29:14 +0700

    Update Card

--- src/app/course/myCourse.tsx
+++ src/app/course/myCourse.tsx
@@ -1,7 +1,13 @@
-'use client'
+"use client";
 
-import { Card, CardBody } from "@nextui-org/card";
+import React from "react";
+import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
+import { Divider } from "@nextui-org/divider";
+import { Image } from "@nextui-org/image";
+import { Link } from "@nextui-org/link";
 import { useState } from "react";
+
+const addHandler = () => {};
 
 const MyCourse = () => {
   return (
@@ -10,12 +16,39 @@ const MyCourse = () => {
         Add Course
       </button>
 
-      <Card>
-        <CardBody>
-          <p>Make beautiful websites regardless of your design experience.</p>
-        </CardBody>
-      </Card>
+      <div className="p-4">
+        <Card className="">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="nextui logo"
+              height={40}
+              radius="sm"
+              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">Username</p>
+              <p className="text-small text-default-500">Email</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://github.com/nextui-org/nextui"
+            >
+              Visit source code on GitHub.
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
 
+      
     </div>
   );
 };
